83f4379aa48bb8f80b3d1cf0ff9ce78baabe2c74
Lukas Rohrer <l.rohrer@pm.me>
2022-07-30 18:32:18 +0200

    finished create and drop queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE songplays"
-user_table_drop = "DROP TABLE users"
-song_table_drop = "DROP TABLE songs"
-artist_table_drop = "DROP TABLE artists"
-time_table_drop = "DROP TABLE time"
+songplay_table_drop = """DROP TABLE IF EXISTS songplays"""
+user_table_drop = """DROP TABLE IF EXISTS users"""
+song_table_drop = """DROP TABLE IF EXISTS songs"""
+artist_table_drop = """DROP TABLE IF EXISTS artists"""
+time_table_drop = """DROP TABLE IF EXISTS time"""
 
 # CREATE TABLES
 
@@ -84,5 +84,5 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
