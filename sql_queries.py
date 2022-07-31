75168f3138b8ccb151dab70a672b403cdefcf12a
Lukas Rohrer <l.rohrer@pm.me>
2022-07-31 18:07:52 +0200

    completet insert and select queries

--- sql_queries.py
+++ sql_queries.py
@@ -9,9 +9,9 @@ time_table_drop = """DROP TABLE IF EXISTS time"""
 # CREATE TABLES
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(
-    songplay_id varchar(255) primary key,
-    start_time bigint references time(start_time),
-    user_id int references users(user_id),
+    songplay_id serial primary key,
+    start_time timestamp references time(start_time),
+    user_id integer references users(user_id),
     level varchar(255),
     song_id varchar(255) references songs(song_id),
     artist_id varchar(255) references artists(artist_id),
@@ -22,7 +22,7 @@ songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(
 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
-    user_id int primary key,
+    user_id integer primary key,
     first_name varchar(255),
     last_name varchar(255),
     gender character (1),
@@ -32,24 +32,24 @@ user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (
     song_id varchar(255) primary key,
-    title varchar(255),
+    title varchar(255) NOT NULL,
     artist_id varchar(255),
     year int,
-    duration double precision
+    duration double precision NOT NULL
 )
 """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
     artist_id varchar(255) primary key,
-    name varchar(255),
+    name varchar(255) NOT NULL,
     location varchar(255),
-    latitude decimal(8,6),
-    longitude decimal(8,6)
+    latitude double precision,
+    longitude double precision
 )
 """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time(
-    start_time bigint primary key,
+    start_time timestamp primary key,
     hour int,
     day int,
     week int,
@@ -61,28 +61,35 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time(
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+
+songplay_table_insert = ("""INSERT INTO songplays (start_time, song_id, artist_id, user_id, level, session_id, location, user_agent) \
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) \
+                            VALUES (%s, %s, %s, %s, %s)
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) \
+                            VALUES (%s, %s, %s, %s, %s)
 """)
 
-artist_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) \
+                            VALUES (%s, %s, %s, %s, %s)
 """)
 
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) \
+                            VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
 
-song_select = ("""
+song_select = ("""SELECT songs.song_id, songs.artist_id FROM artists JOIN songs on artists.artist_id = songs.artist_id \
+                    WHERE songs.title = %s AND songs.duration = %s AND artists.name = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+create_table_queries = [user_table_create, artist_table_create,song_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
