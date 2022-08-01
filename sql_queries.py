da40eca84246dbffc1c7d54d22562d36103402f6
Lukas Rohrer <l.rohrer@pm.me>
2022-08-01 09:58:11 +0200

    added ON CONFLICT clauses

--- sql_queries.py
+++ sql_queries.py
@@ -63,24 +63,24 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time(
 
 
 songplay_table_insert = ("""INSERT INTO songplays (start_time, song_id, artist_id, user_id, level, session_id, location, user_agent) \
-                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) \
-                            VALUES (%s, %s, %s, %s, %s)
+                            VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) \
-                            VALUES (%s, %s, %s, %s, %s)
+                            VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) \
-                            VALUES (%s, %s, %s, %s, %s)
+                            VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 
 time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) \
-                            VALUES (%s, %s, %s, %s, %s, %s, %s)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
